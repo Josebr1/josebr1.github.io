66ee80d566e87cab5acc86428047266efa7471b5
José Antônio <jose.silva.br@outlook.com>
2021-02-22 00:32:08 -0300

    Add Project

---
+++ src/app/skills/data.ts
@@ -0,0 +1,22 @@
+export var single = [
+  {
+    "name": "JavaScript",
+    "value": 85
+  },
+  {
+    "name": "Android",
+    "value": 80
+  },
+  {
+    "name": "Angular",
+    "value": 89
+  },
+  {
+    "name": "Ionic",
+    "value": 60
+  },
+  {
+    "name": "NodeJS",
+    "value": 75
+  }
+];
